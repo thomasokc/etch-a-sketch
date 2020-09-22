629ea4862355174c752960f85bad35fbf4a858d9
Thomas Morris <thomasmorris682@gmail.com>
2020-09-21 18:07:53 -0800

    Add README.md and app.js and index.css and index.html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+Something between a sketch pad and an etch-a-sketch
---
+++ app.js
@@ -0,0 +1,7 @@
+var div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "gray";
+div.style.borderColor = "black";
+
+document.getElementById("container").appendChild(div);
